78783820db8b5f912f68751784ed908280bbbefa
Chidinma <chifineg@gmail.com>
2023-06-09 20:27:12 +0400

    added compound interest program

---
+++ compdInterest.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+    program to calculate compound interest
+    over 10 years
+"""
+# get initial amount from user
+amount = eval(input("enter starting amount "))
+# get rate from user
+rate = eval(input("enter rate "))
+# convert rate to percentage
+rate = rate / 100
+# calculate compound interest
+for i in range(1, 11):
+    amount = amount + (amount * rate)
+    if (i == 1):
+        print("amount in {} year = {:.2f}".format(i, amount))
+    else:
+        print("amount in {} years = {:.2f}".format(i, amount))
+#compound interest after 10 years
+print("Total money compounded in 10 years = ${:.2f}".format(amount))
